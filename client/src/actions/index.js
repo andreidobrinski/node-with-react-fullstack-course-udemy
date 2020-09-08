aa36f936f1d81de026e304a4abac054467c20701
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-08 15:32:39 -0400

    test fetchUser action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => function (dispatch) {
+export const fetchUser = () => function (dispatch) {
   axios.get('/api/current_user')
     .then((res) => dispatch({ type: FETCH_USER, payload: res }));
 };
