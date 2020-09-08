0d11176af87f8517e49ef6785a93f808d6044216
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-08 15:51:49 -0400

    add eslint rule for props destructure

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/prefer-stateless-function': [0],
+    'react/destructuring-assignment': [0],
   },
 };
