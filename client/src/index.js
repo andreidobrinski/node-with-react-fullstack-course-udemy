1444e65ef0f0595a6d20a47653885a0b04f07418
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-08 12:55:18 -0400

    add reducers to index

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
+import reducers from './reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
