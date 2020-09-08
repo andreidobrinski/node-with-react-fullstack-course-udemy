79e8ff05f0a8998660161f36be2f32f1c6b04bac
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-08 12:49:57 -0400

    add redux provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root'),
 );
 
