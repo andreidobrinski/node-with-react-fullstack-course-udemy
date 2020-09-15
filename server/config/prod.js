2586405727d3952f16bdc966ed8b22621d0d321e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-15 19:56:57 -0400

    update survey template

--- server/config/prod.js
+++ server/config/prod.js
@@ -7,4 +7,5 @@ module.exports = {
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
   sgEmail: process.env.SG_EMAIL,
+  redirectDomain: process.env.REDIRECT_DOMAIN,
 };
