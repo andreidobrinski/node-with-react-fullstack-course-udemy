7ab2e25558ab97d713c9d33d78449c2b6804652e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-21 15:01:55 -0400

    add onCancel to SurveyFormReview

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const SurveyReview = () => (
+const SurveyFormReview = ({ onCancel }) => (
   <div>
     <h5>Please confirm your entries</h5>
+    <button
+      onClick={onCancel}
+      className="yellow darken-3 btn-flat"
+    >
+      Back
+    </button>
   </div>
 );
 
-export default SurveyReview;
+export default SurveyFormReview;
