57d816e39959ceec56a3d9f43d03d5cfb62cc8f4
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-22 13:54:33 -0400

    update last responded date

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -41,6 +41,7 @@ module.exports = (app) => {
           {
             $inc: { [choice]: 1 },
             $set: { 'recipients.$.responded': true },
+            lastResponded: new Date(),
           },
         ).exec();
       })
