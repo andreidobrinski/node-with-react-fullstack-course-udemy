dbe4d8ca7e81b699f4646d6e06f814ee9e51cdd0
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-22 14:45:24 -0400

    fix survey list styling

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -11,7 +11,7 @@ class SurveyList extends Component {
     return this.props.surveys
       .reverse()
       .map((survey) => (
-        <div className="card darken-1 blue-grey white-text" key={survey._id}>
+        <div className="card darken-1 white-text" key={survey._id}>
           <div className="card-content">
             <span className="card-title">{survey.title}</span>
             <p>{survey.body}</p>
