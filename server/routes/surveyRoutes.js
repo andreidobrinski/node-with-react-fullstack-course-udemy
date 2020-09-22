8db167169d100bf66c7c7b90dc9723bf9986841c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-22 13:49:11 -0400

    add mongo query to update survey

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url));
         if (match) {
@@ -30,6 +30,20 @@ module.exports = (app) => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email, responded: false },
+            },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+          },
+        ).exec();
+      })
       .value();
 
     res.send({});
